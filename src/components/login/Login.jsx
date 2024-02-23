f7968e9b6938bb4d41a0d6f59f7b47730cab0bb5
JKuriata <JK271096@outlook.com>
2024-02-23 10:20:15 +0000

    enhancement: login functional

--- src/components/login/Login.jsx
+++ src/components/login/Login.jsx
@@ -3,13 +3,32 @@ import { useState } from "react";
 import { login } from "../../utils/fetch";
 
 const Login = () => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  const changeHander = (e, setter, state) => {
+    setter(e.target.value);
+    console.log(state);
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    console.log("Hello from handleSubmit");
+    await login(username, password);
+  };
   return (
     <div>
       <h3>Login</h3>
-      <form>
-        <input placeholder="Username" />
-        <input placeholder="Password" />
-        <button>Login</button>
+      <form onSubmit={handleSubmit}>
+        <input
+          placeholder="Username"
+          onChange={(e) => changeHander(e, setUsername, username)}
+        />
+        <input
+          placeholder="Password"
+          onChange={(e) => changeHander(e, setPassword, password)}
+        />
+        <button type="submit">Login</button>
       </form>
     </div>
   );
